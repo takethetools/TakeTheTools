143505c99d7ee8e8b858357aaa76155d06f09e66
TaheThe Tools <takethetools7@gmail.com>
2026-03-13 05:50:50 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+export const runtime = 'edge';
 
 /**
  * Middleware to block common malicious scan paths and WordPress probing attempts.
